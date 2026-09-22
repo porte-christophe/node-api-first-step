3e7b168f41126012795ab04b492d2dc808538672
Christophe Porte <porte17christophe69@gmail.com>
2026-09-22 10:47:18 +0200

    ajout des cors

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv';
 import Express from "express";
-import salleRouter from "./src/routers/salle.router.ts"
+import salleRouter from "./src/routers/salle.router.ts";
+import cors from "cors";
+
 
 dotenv.config();
 
@@ -9,7 +11,11 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:5173",
+}));
 app.use("/", salleRouter);
+
 
 
 app.listen(port, ()=>{
